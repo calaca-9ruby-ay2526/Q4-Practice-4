eff9265e0060b2a689b4f9ebac3e8fe1d5d5faed
calaca-9ruby-ay2526 <calaca.mm@obmontessori.edu.ph>
2026-04-24 12:40:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function count() {
+
+    var animals = ["Lion", "Elephant", "Dinosaur", "Monkey"];
+
+    for (var i = 0; i < animals.length; i++) {
+        window.alert("This zoo contains a " + animals[i] + ".");
+    }
+}
